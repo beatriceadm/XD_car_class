e607e0c02384e635b34a37635d187118c10e6b56
beatriceadm <118632627+beatriceadm@users.noreply.github.com>
2023-10-31 14:53:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XD_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "white", 2021, 100);
+            Car Opel = new Car("Opel", "blue", 2020, 120);
+            Car Volvo = new Car("XC-90");
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.year);
+            Console.WriteLine(Ford.color);
+            Ford.fullThrottle();
+
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.year);
+            Console.WriteLine(Opel.color);
+            Opel.fullThrottle();
+            Console.ReadKey();
+        }
+    }
+}
